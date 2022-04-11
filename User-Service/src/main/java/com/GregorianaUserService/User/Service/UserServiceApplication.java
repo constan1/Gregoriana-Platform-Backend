cc0ebffef59c4c0f0a2c27798e0ac687f0e251ea
Andrei <andrei_liquid@hotmail.com>
2022-04-11 15:51:54 -0400

    Set up Services - TO DO : API GATEWAY
    Set up Services - TO DO : API GATEWAY

--- User-Service/src/main/java/com/GregorianaUserService/User/Service/UserServiceApplication.java
+++ User-Service/src/main/java/com/GregorianaUserService/User/Service/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.GregorianaUserService.User.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
