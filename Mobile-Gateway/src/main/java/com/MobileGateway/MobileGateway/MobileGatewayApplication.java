f4db70e89740e3674af53a70ffb296fe48e86b78
constan1 <60414383+constan1@users.noreply.github.com>
2022-04-24 00:27:29 -0400

    configurations
    configurations

---
+++ Mobile-Gateway/src/main/java/com/MobileGateway/MobileGateway/MobileGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.MobileGateway.MobileGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileGatewayApplication.class, args);
+	}
+
+}
---
+++ Mobile-Gateway/src/test/java/com/MobileGateway/MobileGateway/MobileGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MobileGateway.MobileGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
