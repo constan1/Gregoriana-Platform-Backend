7ab9699a2e3a605f273e8a868bd98af5d6397ee0
constan1 <60414383+constan1@users.noreply.github.com>
2022-05-24 15:28:37 -0400

    Update CustomerRepository.java

--- Request-Service/src/main/java/com/RequestService/Request/Service/Repository/CustomerRepository.java
+++ Request-Service/src/main/java/com/RequestService/Request/Service/Repository/CustomerRepository.java
@@ -3,13 +3,12 @@ package com.RequestService.Request.Service.Repository;
 
 import com.RequestService.Request.Service.Model.Consumers.privateRequest.TransportRequests;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.LockModeType;
+
 import javax.transaction.Transactional;
 import java.util.List;
 
