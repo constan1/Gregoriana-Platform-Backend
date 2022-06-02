bdafe923b3f3986396f0e8ebd62c3db9f2d77a70
Andrei <andrei_liquid@hotmail.com>
2022-06-01 23:51:16 -0400

    updated docker image
    updated docker image - removed vault

--- User-Service/src/main/java/com/GregorianaUserService/User/Service/config/vaultConfiguration.java
+++ User-Service/src/main/java/com/GregorianaUserService/User/Service/config/vaultConfiguration.java
@@ -2,16 +2,16 @@ package com.GregorianaUserService.User.Service.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "vault")
+@ConfigurationProperties(prefix = "encryp")
 public class vaultConfiguration {
 
-    private static String keyValue;
+    private static String ePassword;
 
     public void setKeyValue(String keyValue) {
-        vaultConfiguration.keyValue = keyValue;
+        vaultConfiguration.ePassword = keyValue;
     }
 
     public static  String getKeyValue() {
-        return keyValue;
+        return ePassword;
     }
 }
