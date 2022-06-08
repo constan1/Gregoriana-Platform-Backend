8ed0139677114983646fff9aa1cecc56d110e64b
constan1 <60414383+constan1@users.noreply.github.com>
2022-06-07 23:14:18 -0400

    configured kubernetes cluster
    configured kubernetes cluster

--- Mobile-Gateway/src/main/java/com/MobileGateway/MobileGateway/MobileGatewayApplication.java
+++ Mobile-Gateway/src/main/java/com/MobileGateway/MobileGateway/MobileGatewayApplication.java
@@ -3,9 +3,11 @@ package com.MobileGateway.MobileGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class MobileGatewayApplication {
 
 	public static void main(String[] args) {
--- Request-Service/src/main/java/com/RequestService/Request/Service/RequestServiceApplication.java
+++ Request-Service/src/main/java/com/RequestService/Request/Service/RequestServiceApplication.java
@@ -2,13 +2,14 @@ package com.RequestService.Request.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
 @EnableEurekaClient
+@RestController
 public class RequestServiceApplication {
 
 	public static void main(String[] args) {
