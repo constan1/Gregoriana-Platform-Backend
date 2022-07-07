4bbc13d25b1736604198c94e30af8ce67cdbc84f
constan1 <60414383+constan1@users.noreply.github.com>
2022-07-06 23:03:11 -0400

    commit

--- Address-Service/src/main/java/com/AddressService/AddressService/AddressServiceApplication.java
+++ Address-Service/src/main/java/com/AddressService/AddressService/AddressServiceApplication.java
@@ -3,11 +3,13 @@ package com.AddressService.AddressService;
 import com.AddressService.AddressService.config.vaultConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties(vaultConfiguration.class)
+
 @EnableEurekaClient
 public class AddressServiceApplication {
 
--- Documents-Service/src/main/java/com/Document/Customer/Service/DocumentsService/DocumentsServiceApplication.java
+++ Documents-Service/src/main/java/com/Document/Customer/Service/DocumentsService/DocumentsServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @EnableConfigurationProperties(vaultConfiguration.class)
